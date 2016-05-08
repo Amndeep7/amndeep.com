3d0f958ab6b54f72367569a20bce70aff426d040
Amndeep Singh Mann <amndeep.singh.mann@gmail.com>
2016-05-08 21:43:36 +0000

    Added in displaying mothers day poem

--- website.py
+++ website.py
@@ -1,3 +1,4 @@
+from __future__ import division, absolute_import, print_function, unicode_literals
 from flask import Flask, render_template, request
 
 website = Flask(__name__)
@@ -8,8 +9,11 @@ def index():
 
 @website.route('/mothersday2016')
 def mothersday2016():
+	poem = ['The fish has too many bones,', 'and the watermelon too many seeds.']
+	with open('mothersday2016poems/default.txt') as f:
+		poem = f.read().strip().split('\n')
 	balpreet = request.args.get('balpreet', '')
-	return render_template('mothersday2016.html', balpreet=balpreet)
+	return render_template('mothersday2016.html', balpreet=balpreet, poem=poem)
 
 if __name__ == '__main__':
 	website.run(host='0.0.0.0')
