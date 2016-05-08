648eda2129e66721fd570ea0c5bd4b65b4661589
Amndeep Singh Mann <amndeep.singh.mann@gmail.com>
2016-05-08 21:17:34 +0000

    Added mothers day page

--- website.py
+++ website.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 website = Flask(__name__)
 
 @website.route('/')
 def index():
 	return render_template('index.html')	
+
+@website.route('/mothersday2016')
+def mothersday2016():
+	balpreet = request.args.get('balpreet', '')
+	return render_template('mothersday2016.html', balpreet=balpreet)
 
 if __name__ == '__main__':
 	website.run(host='0.0.0.0')
