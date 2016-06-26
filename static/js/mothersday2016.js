b1679c305fb5187167c619a869f63d5d47b96929
Amndeep Singh Mann <amndeep.singh.mann@gmail.com>
2016-06-26 03:46:16 +0000

    Here's all the mother's day stuff getting deleted.

--- static/js/mothersday2016.js
+++
@@ -1,7 +0,0 @@
-function fill_background() {
-	let text = '';
-	for(let i = 0; i < 5000; i++) {
-		text += "HAPPY MOTHER'S DAY ";
-	}
-	$('#backgroundtext').html('<p>' + text + '</p>');
-}	
--- website.py
+++ website.py
@@ -1,22 +1,14 @@
-import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 from settings import APP_ROOT
 
 application = Flask(__name__)
 application.debug = True
 
+
 @application.route('/')
 def index():
-	return render_template('index.html')	
-
-@application.route('/mothersday2016')
-def mothersday2016():
-	poem = ['The fish has too many bones,', 'and the watermelon too many seeds.']
-	with open(os.path.join(APP_ROOT, 'mothersday2016poems/poem1.txt')) as f:
-		poem = f.read().strip().split('\n')
-	balpreet = request.args.get('balpreet', '')
-	return render_template('mothersday2016.html', balpreet=balpreet, poem=poem)
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	application.run(host='0.0.0.0')
